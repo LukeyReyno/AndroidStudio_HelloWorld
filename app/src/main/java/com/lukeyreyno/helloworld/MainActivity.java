9ad3ac94f7eb2201599bc9d62a767e07cea15c63
Lucas Reyna <lucas.reyna143@gmail.com>
2021-07-14 15:58:56 -0700

    some more features to test

--- app/src/main/java/com/lukeyreyno/helloworld/MainActivity.java
+++ app/src/main/java/com/lukeyreyno/helloworld/MainActivity.java
@@ -1,7 +1,8 @@
 package com.lukeyreyno.helloworld;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.constraintlayout.widget.ConstraintLayout;
+import android.widget.Button;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
@@ -17,7 +18,9 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @SuppressLint("SetTextI18n")
-    public void onBtnClick(View v) {
+    public void submit(View v) {
+        ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+
         TextView helloTxt = findViewById(R.id.helloWorldText);
         TextView firstNameText = findViewById(R.id.textViewFirstName);
         TextView lastNameText = findViewById(R.id.textViewLastName);
@@ -31,5 +34,32 @@ public class MainActivity extends AppCompatActivity {
         firstNameText.setText("First Name: " + firstNameInput.getText().toString());
         lastNameText.setText("Last Name: " + lastNameInput.getText().toString());
         emailText.setText("Email: " + emailInput.getText().toString());
+
+        mainLayout.setBackgroundColor(0xFFFFCC);
+
+        v.setEnabled(false); //turns off button used to click
+        Button submitBtn = (Button) v;
+        submitBtn.setBackgroundColor(0xFFCCCC);
+        submitBtn.setText("Disabled");
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void reset(View v) {
+        ConstraintLayout mainLayout = findViewById(R.id.mainLayout);
+        Button submitButton = findViewById(R.id.submitBtn);
+
+        TextView helloTxt = findViewById(R.id.helloWorldText);
+        TextView firstNameText = findViewById(R.id.textViewFirstName);
+        TextView lastNameText = findViewById(R.id.textViewLastName);
+        TextView emailText = findViewById(R.id.textViewEmail);
+
+        helloTxt.setText("Hello World!");
+        firstNameText.setText("First Name");
+        lastNameText.setText("Last Name");
+        emailText.setText("Email");
+
+        mainLayout.setBackgroundColor(0xFFFFFF);
+
+        submitButton.setEnabled(true); //turns on button used to submit
     }
 }
